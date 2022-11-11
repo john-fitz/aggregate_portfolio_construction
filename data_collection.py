125e6717dca056df9df62885fa0166742070c0f0
john-fitz <johnfitz16@gmail.com>
2022-11-11 12:49:02 -0500

    adding function to access API token

--- data_collection.py
+++ data_collection.py
@@ -1,12 +1,27 @@
 import numpy as np
 import pandas as pd
 import os
+import logging
+
+from sec_api import FormNportApi
 
 class DataImport:
     def init(self, fund_holdings_file: str):
         self._fund_holdings_file = fund_holdings_file
         self._API_TOKEN = os.environ['SEC_API_TOKEN']
         
+    def import_API_token(self) -> str:
+        """ tests if API token exists and returns value if it does """
+
+        token_value = ""
+
+        try:
+            token_value = os.environ['SEC_API_TOKEN'] 
+        except KeyError:
+            logging.error("Unable to find sec-api token in environment variables")
+            
+        return token_value
+    
     def individual_fund_holdings_folder(self, CIK: str) -> str:
         """ creates folder for fund holdings if it doesn't exist or returns folder name if it does
 
@@ -22,6 +37,19 @@ class DataImport:
 
     def pull_and_save_fund_holdings(self, CIK: str) -> None:
         """ queries API to pull latest fund holdings and saves as a CSV """
+        
+        nportApi = FormNportApi("INPUT API TOKEN HERE")
+ 
+        response = nportApi.get_data(
+            {
+                "query": {"query_string": {
+                    "query": f"genInfo.regCik:{CIK}"
+                    }
+                },
+            }
+        )
+
+        
     
     def CUSIP_to_ticker(self, CUSIP: str) -> str:
         """ looks up the CUSIP string and returns a ticker for the company that issued that security
