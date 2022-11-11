60c1aabbb2be04b29371090f410e1d4eeb7ad9b4
mangoQueen <yoojin980626@gmail.com>
2022-11-11 13:25:01 -0500

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -4,24 +4,25 @@ import os
 import logging
 
 from sec_api import FormNportApi
+from sec_api import MappingApi
 
 class DataImport:
     def init(self, fund_holdings_file: str):
         self._fund_holdings_file = fund_holdings_file
         self._API_TOKEN = os.environ['SEC_API_TOKEN']
-        
+
     def import_API_token(self) -> str:
         """ tests if API token exists and returns value if it does """
 
         token_value = ""
 
         try:
-            token_value = os.environ['SEC_API_TOKEN'] 
+            token_value = os.environ['SEC_API_TOKEN']
         except KeyError:
             logging.error("Unable to find sec-api token in environment variables")
-            
+
         return token_value
-    
+
     def individual_fund_holdings_folder(self, CIK: str) -> str:
         """ creates folder for fund holdings if it doesn't exist or returns folder name if it does
 
@@ -37,9 +38,9 @@ class DataImport:
 
     def pull_and_save_fund_holdings(self, CIK: str) -> None:
         """ queries API to pull latest fund holdings and saves as a CSV """
-        
+
         nportApi = FormNportApi("INPUT API TOKEN HERE")
- 
+
         response = nportApi.get_data(
             {
                 "query": {"query_string": {
@@ -49,9 +50,9 @@ class DataImport:
             }
         )
 
-        
-    
-    def CUSIP_to_ticker(self, CUSIP: str) -> str:
+
+
+    def CUSIP_to_ticker(self, cusip: str) -> str:
         """ looks up the CUSIP string and returns a ticker for the company that issued that security
 
         Args:
@@ -60,5 +61,6 @@ class DataImport:
         Returns:
             str: ticker of the company that issued that security
         """
-    
-    
+        mappingApi = MappingApi(api_key = self._API_TOKEN)
+        result = mappingApi.resolve("cusip", CUSIP)
+        return result[0]['ticker']
