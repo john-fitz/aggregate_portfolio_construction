5828de3fd4bb28ee9ae8ce646dedad801d183b8c
john-fitz <johnfitz16@gmail.com>
2022-11-18 15:05:31 -0500

    adding import_holdings_df

--- data_collection.py
+++ data_collection.py
@@ -9,18 +9,21 @@ from sec_api import FormNportApi
 class DataImport:
     def init(self, fund_holdings: dict):
         self.fund_holdings = fund_holdings
-        self._API_TOKEN = os.environ['SEC_API_TOKEN']
-        self._nportApi = FormNportApi(self.API_TOKEN)
 
     @property
     def API_TOKEN(self):
-        return self._API_TOKEN
-    
+        token = None
+        try: 
+            token = os.environ['SEC_API_TOKEN']
+        except:
+            logging.error("Cannot find API Token in environment variables")
+        
+        return token
+
     @property
     def nportApi(self):
-        return self._nportApi
-    
-    
+        return FormNportApi(self.API_TOKEN)
+
     def import_API_token(self) -> str:
         """ tests if API token exists and returns value if it does """
 
@@ -86,7 +89,32 @@ class DataImport:
         if correct_filing is None:
             logging.error(f"Unable to locate filing for CIK: {CIK} and Series: {series}")
         else:
-            return correct_filing    
+            return correct_filing 
+    
+    def import_holdings_df(self, CIK: str, series: str) -> pd.DataFrame:
+        """ locates latest filing and returns holdings as a DataFrame
+
+        Args:
+            CIK (str): CIK of filing institution
+            series (str): Series corresponding to the specific being held
+
+        Returns:
+            pd.DataFrame: DataFrame of holdings information
+        """
+        data = {'company_name': [], 'CUSIP': [], 'num_holdings': [], 'invested_amt_usd': [], 'percent_of_portfolio': [], 'country': []}
+
+        holdings = self.query_holdings(CIK, series)
+        
+        if holdings is not None:
+            for holding in holdings['invstOrSecs']:
+                data['company_name'].append(holding['name'])
+                data['CUSIP'].append(holding['cusip'])
+                data['num_holdings'].append(holding['balance'])
+                data['invested_amt_usd'].append(holding['valUSD'])
+                data['percent_of_portfolio'].append(holding['pctVal'])
+                data['country'].append(holding['invCountry'])
+
+        return pd.DataFrame.from_dict(data)
     
     def CUSIP_to_ticker(self, CUSIP: str) -> str:
         """ looks up the CUSIP string and returns a ticker for the company that issued that security
