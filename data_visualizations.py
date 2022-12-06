e9bb2af6508c603522bdc47695511e4d61a9c461
Viththal-K <86553451+Viththal-K@users.noreply.github.com>
2022-12-06 11:42:00 -0500

    Update data_visualizations.py

--- data_visualizations.py
+++ data_visualizations.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 class DataVisualizations:
     def __init__(self, df: pd.DataFrame):
